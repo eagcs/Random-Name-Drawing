a1942f6d49fde0eeb62ee603c4702dd9ac634fde
eagcs <107284248+eagcs@users.noreply.github.com>
2022-06-11 01:51:13 -0400

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,10 +1,4 @@
 var member = [
-{"name" : "Aiden Liu------(SEEDS)"},
-{"name" : "Amy Chen------(SEEDS)"},
-{"name" : "Amy Cheng------(iTeach)"},
-{"name" : "Andrew Dai------(IT, JRC)"},
-{"name" : "Angie Shen------(iTeach, JRC)"},
-{"name" : "Anthea Chu------(IT)"},
-{"name" : "Audrey Dai------(iTeach)"},
-{"name" : "Brendan Xue------(SEEDS)"},
+{"name" : "Aiden Liu",
+ "phone" : "(SEEDS)"}
 ]
